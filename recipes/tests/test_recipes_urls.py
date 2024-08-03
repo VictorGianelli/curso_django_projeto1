5662966659662f500135f04caf8c0f1bb3e52cf6
VictorGianelli <victorgnll@gmail.com>
2024-08-03 10:28:08 -0300

    Testando 404 Not Found quando receitas não existem

--- recipes/tests/test_recipes_urls.py
+++ recipes/tests/test_recipes_urls.py
@@ -15,10 +15,3 @@ class RecipeURLsTest(TestCase):
         url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(url, '/recipes/1/')
     
-    def test_recipe_home_view_returns_status_code_200_OK(self):
-        response = self.client.get(reverse('recipes:home'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_recipe_home_view_loads_correct_template(self):
-        response = self.client.get(reverse('recipes:home'))
-        self.assertTemplateUsed(response, 'recipes/pages/home.html')
