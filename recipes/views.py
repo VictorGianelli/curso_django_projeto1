23473ba414de5c97f39c311a44c941fa3ff28654
VictorGianelli <victorgnll@gmail.com>
2024-08-09 16:02:06 -0300

    Adicionando o paginator na Home

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.core.paginator import Paginator
 from django.http import Http404
 from django.shortcuts import get_list_or_404, render, get_object_or_404
 from django.shortcuts import render
@@ -9,8 +10,13 @@ def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
+
+    current_page = request.GET.get('page', 1)
+    paginator = Paginator(recipes, 9)
+    page_obj = paginator.get_page(current_page)
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
     })
 
 
